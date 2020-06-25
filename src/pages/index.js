0a24f506243a1455e338c0134b7af8bc83651759
shina <rasheedshinaopeyemi@gmail.com>
2020-06-25 23:52:09 +0100

    added navbar, navbar items and navbar toggle functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { FaGulp } from "react-icons/fa"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
