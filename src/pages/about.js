d1db110512cf23605d592e50d989747de85280f2
shina <rasheedshinaopeyemi@gmail.com>
2020-06-26 22:59:48 +0100

    added the about page

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <BackgroundSection
+      img={data.bgImg.childImageSharp.fluid}
+      title="about us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    bgImg: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
