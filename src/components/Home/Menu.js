04533825f3363f469ffef56287b1038988cd7414
shina <rasheedshinaopeyemi@gmail.com>
2020-06-27 14:20:42 +0100

    added functionality to filter coffeitems by categories

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,12 +2,38 @@ import React, { Component } from "react"
 import Title from "../Globals/Title"
 import Img from "gatsby-image"
 
+const getCategories = items => {
+  let tempItems = items.map(item => {
+    return item.node.category
+  })
+
+  let temCategories = new Set(tempItems)
+  let categories = Array.from(temCategories)
+  categories = ["all", ...categories]
+  return categories
+}
+
 export default class Menu extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: props.items.edges,
       coffeeItems: props.items.edges,
+      categories: getCategories(props.items.edges),
+    }
+  }
+
+  handleItems = category => {
+    let tempItems = [...this.state.items]
+    if (category === "all") {
+      this.setState(() => {
+        return { coffeeItems: tempItems }
+      })
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category)
+      this.setState(() => {
+        return { coffeeItems: items }
+      })
     }
   }
   render() {
@@ -17,6 +43,24 @@ export default class Menu extends Component {
           <div className="container">
             <Title title="best of our menu" />
             {/* categories */}
+            <div className="row mb-5">
+              <div className="col-10 auto text-center">
+                {this.state.categories.map((category, index) => {
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className="btn btn-yellow text-capitalize m-3"
+                      onClick={() => {
+                        this.handleItems(category)
+                      }}
+                    >
+                      {category}
+                    </button>
+                  )
+                })}
+              </div>
+            </div>
             {/* items */}
             <div className="row">
               {this.state.coffeeItems.map(({ node }) => {
@@ -31,8 +75,12 @@ export default class Menu extends Component {
 
                     <div className="flex-grow-1 px-3">
                       <div className="d-flex justify-content-between">
-                        <h6 className="mb-0">{node.title}</h6>
-                        <h6 className="mb-0">{node.price}</h6>
+                        <h6 className="mb-0">
+                          <small>{node.title}</small>
+                        </h6>
+                        <h6 className="mb-0 text-yellow">
+                          <small>{node.price}</small>
+                        </h6>
                       </div>
                       <p className="text-muted">
                         <small>{node.description.description}</small>
