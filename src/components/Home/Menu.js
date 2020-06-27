8785503ad1c22a4b49a673d62d0966bc9f18443d
shina <rasheedshinaopeyemi@gmail.com>
2020-06-27 12:50:26 +0100

    fetched the coffeitems from contentful and passed them into a Menu component

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      coffeeItems: props.items.edges,
+    }
+  }
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="best of our menu" />
+            <div className="row mb-5">
+              <div className="col mx-auto">
+                <h1 className="text-center">these are our items</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="best of our menu" />
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center">
+                <h1>there are no items to display</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
